d275eb39d288d1b76a26c2688d554f5b907dbb33
bill fetherstonhaugh <bill.fethers@gmail.com>
2017-11-17 12:42:38 +0800

    no message

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+/*
+
+var age = 16
+
+if age != 19 {
+    print ("We are the same age")
+}
+ */
+
+var name = "nick"
+
+
+
+if name == "Nick" {
+    print ("we have the same name")
+}
+
+else {
+   print ("not the same")
+}
